3cc1f174c7fae2960560773c80e2c27ecbd6a471
Sol95 <solomonkr1995@gmail.com>
2022-10-09 14:02:11 +1100

    FIrst commit

---
+++ snake.js
@@ -0,0 +1,24 @@
+export const snakeSpeed = 1;
+const snakeBody = [
+    { x: 10, y: 11},
+    { x: 11, y: 11},
+    { x: 12, y: 11}
+]
+
+export function update() {
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] }
+    }
+
+    snakeBody[0].x += 1
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
+}
