b9a94362334fb1a9d0d2430b16e4ab52a174d48a
Sol95 <solomonkr1995@gmail.com>
2023-01-28 12:02:38 +1100

    Reverting to 3d4c52f

--- game.js
+++ game.js
@@ -2,24 +2,6 @@ import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, sna
 import { update as updateFood, draw as drawFood } from './food.js'  
 import { outsideGrid } from './grid.js'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDfmiYSSL26oOtB44vw5vVtSWRRgUtQX4o",
-  authDomain: "snake-game-67a5d.firebaseapp.com",
-  projectId: "snake-game-67a5d",
-  storageBucket: "snake-game-67a5d.appspot.com",
-  messagingSenderId: "667134728944",
-  appId: "1:667134728944:web:21b8a8ef5ad5572454c035"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 let lastRenderTime = 0;
 let gameOver = false;
 const gameBoard = document.getElementById('game-board')
