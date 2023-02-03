21844d37095510f08062a1f9553951972226df59
Sol95 <solomonkr1995@gmail.com>
2023-02-03 14:06:07 +1100

    fixed restart button

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ const scoreBoard = document.getElementById('score-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm(`You lost. Your score was ${snakeBody.length - 1}. Press ok to restart.`)) {
-            window.location = '/'
+            window.location = '/Snake-Game/'
         }
         return
     }
